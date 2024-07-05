196b794620523d74482410476aa2f8569b601979
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-07-05 20:01:26 -0300

    Primeiro commit com primeira partes dos exercícios

---
+++ react-nature/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import CardBicicleta from './components/CardBicicleta'
+
+function App() {
+
+  const bicicletas = [
+    {
+      id: 0, nomeModelo: 'Magic Might', preco: 2499,
+      imgSrc: 'https://www.origamid.com/projetos/bikcraft/img/bicicletas/magic-home.jpg',            
+    },
+    {
+      id: 1, nomeModelo: 'Nimbus Stark', preco: 4999,
+      imgSrc: 'https://www.origamid.com/projetos/bikcraft/img/bicicletas/nimbus-home.jpg',            
+    },
+    {
+      id: 2, nomeModelo: 'Nebula Cosmic', preco: 3999,
+      imgSrc: 'https://www.origamid.com/projetos/bikcraft/img/bicicletas/nebula-home.jpg',      
+    }
+  ]
+
+  return (
+    <>
+      <Header></Header>      
+
+      {bicicletas.map(bike =>
+        <CardBicicleta 
+          key={bike.id} 
+          imgSrc={bike.imgSrc} 
+          preco={bike.preco} 
+          nomeModelo={bike.nomeModelo}/>
+      )}
+
+      <Footer></Footer>      
+    </>
+  )
+}
+
+export default App
---
+++ react-nature/src/Contato.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function Contato() {
+    return (
+        <>
+            <Header></Header>
+
+            <h1>Contato</h1>
+            <textarea></textarea>
+            <button>Enviar</button>
+
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default Contato
